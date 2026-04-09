9986f7e2ead5a5b2b9f2ef9a57dee21e3680bdde
Steve <stigzler@msn.com>
2026-04-09 14:38:42 +0100

    viewmodels done

---
+++ ROMVault/ViewModels/GameInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROMVault.ViewModels
+{
+    internal class GameInfo
+    {
+        public string Category { get; set; } = string.Empty;
+        public string CloneOf { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Manufacturer { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string RomOf { get; set; } = string.Empty;
+        public string TrupipYear { get; set; } = string.Empty;
+        public string TruripCloneOf { get; set; } = string.Empty;
+        public string TruripDeveloper { get; set; } = string.Empty;
+        public string TruripGenre { get; set; } = string.Empty;
+        public string TruripID { get; set; } = string.Empty;
+        public string TruripPlayers { get; set; } = string.Empty;
+        public string TruripPublisher { get; set; } = string.Empty;
+        public string TruripRatings { get; set; } = string.Empty;
+        public string TruripRelatedTo { get; set; } = string.Empty;
+        public string TruripScore { get; set; } = string.Empty;
+        public string TruripSource { get; set; } = string.Empty;
+        public string TruripSubGenre { get; set; } = string.Empty;
+        public string Year { get; set; } = string.Empty;
+    }
+}
