1a4b3d1b4f83f9477ca40f587175a9184c2562ad
Steve <stigzler@msn.com>
2026-04-10 12:16:10 +0100

    GameINfo update checking (pre label/textbox delete)

--- ROMVault/FrmMain_GameMetaData.cs
+++ ROMVault/FrmMain_GameMetaData.cs
@@ -48,8 +48,6 @@ namespace ROMVault
 
         private void AddGameMetaData()
         {
-            //AddTextBox(2, "Manufacturer", 6, 84, out _labelGameManufacturer, out _textGameManufacturer);
-
             //AddTextBox(3, "Clone of", 6, 84, out _labelGameCloneOf, out _textGameCloneOf);
             //AddTextBox(3, "Year", 206, 284, out _labelGameYear, out _textGameYear);
 
@@ -57,9 +55,6 @@ namespace ROMVault
             //AddTextBox(4, "Category", 206, 284, out _labelGameCategory, out _textGameCategory);
 
             //Trurip
-
-            //AddTextBox(2, "Publisher", 6, 84, out _labelTruripPublisher, out _textTruripPublisher);
-            //AddTextBox(2, "Title Id", 406, 484, out _labelTruripTitleId, out _textTruripTitleId);
 
             //AddTextBox(3, "Developer", 6, 84, out _labelTruripDeveloper, out _textTruripDeveloper);
             //AddTextBox(3, "Source", 406, 484, out _labelTruripSource, out _textTruripSource);
@@ -85,126 +80,42 @@ namespace ROMVault
         private void UpdateGameMetaData(RvFile tGame)
         {
             GameInfo.Name = tGame.Name;
-
-            _labelGameName.Visible = true;
             string gameId = tGame.Game?.GetData(RvGame.GameData.Id);
 
             if (!string.IsNullOrWhiteSpace(gameId))
                 GameInfo.Name += $" (ID:{gameId})";
 
-            if (tGame.Game == null || tGame.Game.GetData(RvGame.GameData.EmuArc) != "yes")
-            {
-                _labelTruripPublisher.Visible = false;
-                _textTruripPublisher.Visible = false;
-
-                _labelTruripDeveloper.Visible = false;
-                _textTruripDeveloper.Visible = false;
-
-                _labelTruripTitleId.Visible = false;
-                _textTruripTitleId.Visible = false;
-
-                _labelTruripSource.Visible = false;
-                _textTruripSource.Visible = false;
-
-                _labelTruripCloneOf.Visible = false;
-                _textTruripCloneOf.Visible = false;
-
-                _labelTruripRelatedTo.Visible = false;
-                _textTruripRelatedTo.Visible = false;
-
-                _labelTruripYear.Visible = false;
-                _textTruripYear.Visible = false;
-
-                _labelTruripPlayers.Visible = false;
-                _textTruripPlayers.Visible = false;
-
-                _labelTruripGenre.Visible = false;
-                _textTruripGenre.Visible = false;
-
-                _labelTruripSubGenre.Visible = false;
-                _textTruripSubGenre.Visible = false;
-
-                _labelTruripRatings.Visible = false;
-                _textTruripRatings.Visible = false;
-
-                _labelTruripScore.Visible = false;
-                _textTruripScore.Visible = false;
-            }
-
-            if (tGame.Game == null || tGame.Game.GetData(RvGame.GameData.EmuArc) == "yes")
-            {
-                _labelGameManufacturer.Visible = false;
-                _textGameManufacturer.Visible = false;
-
-                _labelGameCloneOf.Visible = false;
-                _textGameCloneOf.Visible = false;
-
-                _labelGameRomOf.Visible = false;
-                _textGameRomOf.Visible = false;
-
-                _labelGameYear.Visible = false;
-                _textGameYear.Visible = false;
-
-                _labelGameCategory.Visible = false;
-                _textGameCategory.Visible = false;
-            }
-
             if (tGame.Game != null)
             {
                 if (tGame.Game.GetData(RvGame.GameData.EmuArc) == "yes")
                 {
-                    _labelGameDescription.Visible = true;
                     string desc = tGame.Game.GetData(RvGame.GameData.Description);
                     if (desc == "¤") desc = Path.GetFileNameWithoutExtension(tGame.Name);
                     GameInfo.Description = desc;
 
-                    _labelTruripPublisher.Visible = true;
-                    _textTruripPublisher.Visible = true;
-                    _textTruripPublisher.Text = tGame.Game.GetData(RvGame.GameData.Publisher);
+                    GameInfo.TruripPublisher = tGame.Game.GetData(RvGame.GameData.Publisher);
 
-                    _labelTruripDeveloper.Visible = true;
-                    _textTruripDeveloper.Visible = true;
-                    _textTruripDeveloper.Text = tGame.Game.GetData(RvGame.GameData.Developer);
+                    GameInfo.TruripDeveloper = tGame.Game.GetData(RvGame.GameData.Developer);
 
-                    _labelTruripTitleId.Visible = true;
-                    _textTruripTitleId.Visible = true;
-                    _textTruripTitleId.Text = tGame.Game.GetData(RvGame.GameData.Id);
+                    GameInfo.TruripID = tGame.Game.GetData(RvGame.GameData.Id);
 
-                    _labelTruripSource.Visible = true;
-                    _textTruripSource.Visible = true;
-                    _textTruripSource.Text = tGame.Game.GetData(RvGame.GameData.Source);
+                    GameInfo.TruripSource = tGame.Game.GetData(RvGame.GameData.Source);
 
-                    _labelTruripCloneOf.Visible = true;
-                    _textTruripCloneOf.Visible = true;
-                    _textTruripCloneOf.Text = tGame.Game.GetData(RvGame.GameData.CloneOf);
+                    GameInfo.CloneOf = tGame.Game.GetData(RvGame.GameData.CloneOf);
 
-                    _labelTruripRelatedTo.Visible = true;
-                    _textTruripRelatedTo.Visible = true;
-                    _textTruripRelatedTo.Text = tGame.Game.GetData(RvGame.GameData.RelatedTo);
+                    GameInfo.TruripRelatedTo = tGame.Game.GetData(RvGame.GameData.RelatedTo);
 
-                    _labelTruripYear.Visible = true;
-                    _textTruripYear.Visible = true;
-                    _textTruripYear.Text = tGame.Game.GetData(RvGame.GameData.Year);
+                    GameInfo.TrupipYear = tGame.Game.GetData(RvGame.GameData.Year);
 
-                    _labelTruripPlayers.Visible = true;
-                    _textTruripPlayers.Visible = true;
-                    _textTruripPlayers.Text = tGame.Game.GetData(RvGame.GameData.Players);
+                    GameInfo.TruripPlayers = tGame.Game.GetData(RvGame.GameData.Players);
 
-                    _labelTruripGenre.Visible = true;
-                    _textTruripGenre.Visible = true;
-                    _textTruripGenre.Text = tGame.Game.GetData(RvGame.GameData.Genre);
+                    GameInfo.TruripGenre = tGame.Game.GetData(RvGame.GameData.Genre);
 
-                    _labelTruripSubGenre.Visible = true;
-                    _textTruripSubGenre.Visible = true;
-                    _textTruripSubGenre.Text = tGame.Game.GetData(RvGame.GameData.SubGenre);
+                    GameInfo.TruripSubGenre = tGame.Game.GetData(RvGame.GameData.SubGenre);
 
-                    _labelTruripRatings.Visible = true;
-                    _textTruripRatings.Visible = true;
-                    _textTruripRatings.Text = tGame.Game.GetData(RvGame.GameData.Ratings);
+                    GameInfo.TruripRatings = tGame.Game.GetData(RvGame.GameData.Ratings);
 
-                    _labelTruripScore.Visible = true;
-                    _textTruripScore.Visible = true;
-                    _textTruripScore.Text = tGame.Game.GetData(RvGame.GameData.Score);
+                    GameInfo.TruripScore = tGame.Game.GetData(RvGame.GameData.Score);
 
                     LoadTruRipPannel(tGame);
                 }
@@ -244,30 +155,19 @@ namespace ROMVault
                     if (!found)
                         HidePannel();
 
-                    _labelGameDescription.Visible = true;
                     string desc = tGame.Game.GetData(RvGame.GameData.Description);
                     if (desc == "¤") desc = Path.GetFileNameWithoutExtension(tGame.Name);
                     GameInfo.Description = desc;
 
-                    _labelGameManufacturer.Visible = true;
-                    _textGameManufacturer.Visible = true;
-                    _textGameManufacturer.Text = tGame.Game.GetData(RvGame.GameData.Manufacturer);
+                    GameInfo.Manufacturer = tGame.Game.GetData(RvGame.GameData.Manufacturer);
 
-                    _labelGameCloneOf.Visible = true;
-                    _textGameCloneOf.Visible = true;
-                    _textGameCloneOf.Text = tGame.Game.GetData(RvGame.GameData.CloneOf);
+                    GameInfo.CloneOf = tGame.Game.GetData(RvGame.GameData.CloneOf);
 
-                    _labelGameRomOf.Visible = true;
-                    _textGameRomOf.Visible = true;
-                    _textGameRomOf.Text = tGame.Game.GetData(RvGame.GameData.RomOf);
+                    GameInfo.RomOf = tGame.Game.GetData(RvGame.GameData.RomOf);
 
-                    _labelGameYear.Visible = true;
-                    _textGameYear.Visible = true;
-                    _textGameYear.Text = tGame.Game.GetData(RvGame.GameData.Year);
+                    GameInfo.Year = tGame.Game.GetData(RvGame.GameData.Year);
 
-                    _labelGameCategory.Visible = true;
-                    _textGameCategory.Visible = true;
-                    _textGameCategory.Text = tGame.Game.GetData(RvGame.GameData.Category);
+                    GameInfo.Category = tGame.Game.GetData(RvGame.GameData.Category);
                 }
             }
             else
@@ -276,84 +176,6 @@ namespace ROMVault
             }
 
             this.ActiveControl = GameGrid;
-        }
-
-        private void gbSetInfo_Resize(object sender, EventArgs e)
-        {
-            const int leftPos = 84;
-            // int rightPos = gbSetInfo.Width - 15;
-            int rightPos = 200; // dummy
-
-            if (rightPos > 750)
-            {
-                rightPos = 750;
-            }
-
-            int width = rightPos - leftPos;
-
-            if (GameInfo.Name == null)
-            {
-                return;
-            }
-
-            // main Meta Data
-            int textWidth = (int)((double)width * 120 / 340);
-            int text2Left = leftPos + width - textWidth;
-            int label2Left = text2Left - 78;
-
-            //reinstate
-            _textGameManufacturer.Width = width;
-
-            _textGameCloneOf.Width = textWidth;
-
-            _labelGameYear.Left = label2Left;
-            _textGameYear.Left = text2Left;
-            _textGameYear.Width = textWidth;
-
-            _textGameRomOf.Width = textWidth;
-
-            _labelGameCategory.Left = label2Left;
-            _textGameCategory.Left = text2Left;
-            _textGameCategory.Width = textWidth;
-
-            // TruRip Meta Data
-            textWidth = (int)(width * 0.20);
-            text2Left = (int)(width * 0.4 + leftPos);
-            label2Left = text2Left - 78;
-            int text3Left = leftPos + width - textWidth;
-            int label3Left = text3Left - 78;
-
-            _textTruripPublisher.Width = (int)(width * 0.6);
-            _textTruripDeveloper.Width = (int)(width * 0.6);
-            _textTruripCloneOf.Width = width;
-            _textTruripRelatedTo.Width = width;
-
-            _textTruripYear.Width = textWidth;
-            _textTruripPlayers.Width = textWidth;
-
-            _labelTruripGenre.Left = label2Left;
-            _textTruripGenre.Left = text2Left;
-            _textTruripGenre.Width = textWidth;
-
-            _labelTruripSubGenre.Left = label2Left;
-            _textTruripSubGenre.Left = text2Left;
-            _textTruripSubGenre.Width = textWidth;
-
-            _labelTruripTitleId.Left = label3Left;
-            _textTruripTitleId.Left = text3Left;
-            _textTruripTitleId.Width = textWidth;
-
-            _labelTruripSource.Left = label3Left;
-            _textTruripSource.Left = text3Left;
-            _textTruripSource.Width = textWidth;
-
-            _labelTruripRatings.Left = label3Left;
-            _textTruripRatings.Left = text3Left;
-            _textTruripRatings.Width = textWidth;
-
-            _labelTruripScore.Left = label3Left;
-            _textTruripScore.Left = text3Left;
-            _textTruripScore.Width = textWidth;
         }
     }
 }
